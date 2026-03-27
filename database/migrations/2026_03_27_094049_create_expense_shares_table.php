43e3cad3ad41210614d4f7c6fa0ce1aa7a09914e
4db444 <brahim77990alhyan@gmail.com>
2026-03-27 11:57:25 +0100

    creating the migration files

---
+++ database/migrations/2026_03_27_094049_create_expense_shares_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense_shares', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("amount", 10, 2);
+
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("group_id");
+            $table->unsignedBigInteger("expense_id");
+
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->foreign("group_id")->references("id")->on("groups")->onDelete("cascade");
+            $table->foreign("expense_id")->references("id")->on("expenses");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense_shares');
+    }
+};
