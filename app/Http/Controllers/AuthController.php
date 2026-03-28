094115d9d3c73b013f966536f0c1815245f56ef4
4db444 <brahim77990alhyan@gmail.com>
2026-03-28 18:53:22 +0100

    creating the auth controller which handles the authenticatoin process of the user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegistrationRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showLoginPage(){
+        return view("auth.login");
+    }
+
+    public function showRegistrationPage () {
+        return view("auth.signup");
+    }
+
+    public function login (UserLoginRequest $request) {
+        if(Auth::attempt($request->validated())){
+            $request->session()->regenerate();
+
+            return redirect()->route("dashboard");
+        }
+
+        return redirect()->back()->withErrors([
+            "wrong credentials !"
+        ]);
+    }
+
+    public function register (UserRegistrationRequest $request) {
+        User::create([
+            ...$request->validated(),
+            "password" => Hash::make($request->safe()->password),
+            "role" => "user"
+        ]);
+
+        return redirect()->route("login");
+    }
+
+    public function logout (Request $request) {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect("/auth/login");
+    }
+}
