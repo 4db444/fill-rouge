a28ebc3baa2899a3bdd3c6c7273ad30f7af6066f
4db444 <brahim77990alhyan@gmail.com>
2026-03-28 18:51:02 +0100

    creating the base layout for the application

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>myapp</h1>
+        @auth
+            <form action="{{ route("logout") }}" method="POST">
+                @csrf
+                <button type="submit">logout</button>
+            </form>
+        @endauth
+    </header>
+
+    @yield("main")
+
+    <footer>
+        <h1>footer</h1>
+    </footer>
+</body>
+</html>
