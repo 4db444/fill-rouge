1aa2b6ff2053cd0c4b135bb1f99d0d1401d5470a
4db444 <brahim77990alhyan@gmail.com>
2026-03-28 18:53:48 +0100

    creating the neccessasry routes for the authenticatoin and also for the dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("/auth")->group(function () {
+    Route::get("/login", [AuthController::class, "showLoginPage"])->name("login");
+    Route::get("/signup", [AuthController::class, "showRegistrationPage"]);
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::post("/signup", [AuthController::class, "register"]);
+    Route::post("/logout", [AuthController::class, "logout"])->name("logout");
+});
+
+Route::middleware("auth")->group(function() {
+    Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
 });
