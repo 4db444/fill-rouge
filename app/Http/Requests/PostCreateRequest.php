0f0b6f37f6189c879d37a72ce584495b9c1a493d
4db444 <brahim77990alhyan@gmail.com>
2026-04-13 15:04:28 +0100

    creating the form request to validate the post creation

---
+++ app/Http/Requests/PostCreateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostCreateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            "title" => "required|string|max:250",
+            "content" => "required|string|max:1000",
+            "address" => "nullable|string|max:250"
+        ];
+    }
+}
