29c2be37c3b923a72c09027b9a9349accfc9f3f2
4db444 <brahim77990alhyan@gmail.com>
2026-04-13 15:05:57 +0100

    creating the routes and also seeder/foactory for the post

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            "title" => fake()->sentence(5, true),
+            "content" => fake()->paragraph(30, true),
+            "address" => fake()->address(),
+            "user_id" => User::inRandomOrder()->first()->id
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
@@ -19,11 +20,22 @@ Route::prefix("/auth")->group(function () {
 });
 
 Route::middleware("auth")->group(function() {
+    // dashboard route
     Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
-    Route::put("/profile/{user}/info", [UserController::class, "updateInfo"])->name("user.info.update");
-    Route::put("/profile/{user}/password", [UserController::class, "updatePassword"])->name("user.password.update");
-    Route::get("/profile/{user?}", [UserController::class, "show"])
-        ->missing(function () {
-            return redirect()->route("dashboard");
-        });
+
+    // profile routes
+    Route::prefix("/profile")->group(function() {
+        Route::put("/{user}/info", [UserController::class, "updateInfo"])->name("user.info.update");
+        Route::put("/{user}/password", [UserController::class, "updatePassword"])->name("user.password.update");
+        Route::get("/{user?}", [UserController::class, "show"])
+            ->missing(function () {
+                return redirect()->route("dashboard");
+            });
+    });
+
+    // post routes
+    Route::prefix("/posts")->group (function () {
+        Route::post("/", [PostController::class, "store"])->name("post.create");
+        Route::post("/{post}/toggle_like", [PostController::class, "toggle_like"])->name("post.toggle_like");
+    });
 });
