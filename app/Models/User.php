a5488356a5ea655aa7c6677659c66975861481cf
4db444 <brahim77990alhyan@gmail.com>
2026-04-16 23:42:42 +0100

    adding the profile relationship to the user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -61,5 +62,9 @@ class User extends Authenticatable
             "user_id",
             "post_id"
         );
+    }
+
+    public function profile () : MorphOne {
+        return $this->morphOne(Image::class, "imageable");
     }
 }
